05771185506c6bdf164677a03e16cfc4ee0dc7c7
JayPatel1314 <69890535+JayPatel1314@users.noreply.github.com>
2020-12-11 19:48:59 +0530

    Update admin.py

--- ItachiRobot main/Modules/admin.py
+++ ItachiRobot main/Modules/admin.py
@@ -5,18 +5,18 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from SaitamaRobot import DRAGONS, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import (bot_admin, can_pin,
+from ItachiRobot main import DRAGONS, dispatcher
+from ItachiRobot main.modules.disable import DisableAbleCommandHandler
+from ItachiRobot main.modules.helper_funcs.chat_status import (bot_admin, can_pin,
                                                            can_promote,
                                                            connection_status,
                                                            user_admin,
                                                            ADMIN_CACHE)
 
-from SaitamaRobot.modules.helper_funcs.extraction import (extract_user,
+from ItachiRobot main.modules.helper_funcs.extraction import (extract_user,
                                                           extract_user_and_text)
-from SaitamaRobot.modules.log_channel import loggable
-from SaitamaRobot.modules.helper_funcs.alternate import send_message
+from ItachiRobot main.modules.log_channel import loggable
+from ItachiRobot main.modules.helper_funcs.alternate import send_message
 
 
 @run_async
